f03934fd8820f84d972baa568c136afff3dc09a3
MichaelDV-Valerian <michael.devin.aryono@gmail.com>
2025-04-15 16:34:39 +0700

    error js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 let homeName = prompt("Siapakah nama anda?", "")
 document.getElementById('home-name').innerHTML = homeName
 
+function validateForm(){
+    const name = document.forms["form-message"]["input-nama"].value;
+    const birthDate = document.forms["form-message"]["input-tanggal-lahir"].value;
+    const gender = document.forms["form-message"]["jenis-kelamin"].value;
+    const message = document.forms["form-message"]["input-pesan"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || message == ""){
+        alert("Tidak boleh ada yang kososng!");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, message);
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, message){
+    document.getElementById("output-nama").innerHTML = name;
+    document.getElementById("output-tanggal-lahir").innerHTML = birthDate;
+    document.getElementById("output-jenis-kelamin").innerHTML = gender;
+    document.getElementById("output-pesan").innerHTML = message;
+}
+
+
+
+
+
 
